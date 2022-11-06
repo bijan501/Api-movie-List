950e7f4272c6edb4c0405bd8c7005074027831b0
Binaya khadka <binaya.bijan501@gmail.comgit commit -m first>
2022-11-06 14:10:47 +1100

    Modified

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,5 +1,6 @@
 import React from "react";
 
 export const Title = () => {
-  return <h2 className="mt-5 text-center mb-2">My movie list</h2>;
+  return <h2 className="mt-5 text-center mb-2">My 
+  Movie list</h2>;
 };
